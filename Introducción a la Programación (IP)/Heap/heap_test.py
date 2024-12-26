990fec3e9a4e7bc9e5e633262b1daa79ae1ce196
santidrele2000 <santidrelewicz2000@gmail.com>
2024-12-26 00:54:58 -0300

    subo una carpeta de prueba

---
+++ Introducción a la Programación (IP)/Heap/heap_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 20:51:22 2024
+
+@author: publico
+"""
+
+import heap
+
+h = heap.Heap()
+for i in range(1, 10):
+    h.agregar(i)
+    h.
+    
