d095cbb65b02584547758e503c1f6f91e30b5ed6
SantiDrelewicz <santidrelewicz2000@gmail.com>
2025-08-27 21:33:29 -0300

    Refactor activation function and update weight calculations in XOR neural network

--- Métodos Avanzados de Síntesis y Procesamiento de Imágenes/ Clase 2 - Redes Neuronales Archivo/Recursos Taller 2/ej1/xor.py
+++ Métodos Avanzados de Síntesis y Procesamiento de Imágenes/ Clase 2 - Redes Neuronales Archivo/Recursos Taller 2/ej1/xor.py
@@ -13,13 +13,14 @@ def activation(x):
     x = b*x
     t=(np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
     dt=(1-t**2)
-    return t,b*dt
+    return t, b*dt
 
 # La entrada consiste en un array cuyas columnas son los ejemplos de
 # aprendizaje y cuyas lineas son los descriptores
-X = np.array([[1, 1, -1, -1],[ 1, -1, 1, -1]])
+X = np.array([[1, 1, -1, -1], 
+              [ 1, -1, 1, -1]])
 [Ni, M]          = X.shape
-No		         = 1
+No		           = 1
 Nh               = 2 
 Theta            = 10e-3
 eta              = 0.25 # ratio de aprendizaje
@@ -50,38 +51,38 @@ while m < epochs:
         tk = T[i]
         # Forward propagation desde la entrada X
         # Calcular primero las aj en las neuronas escondidas
-        aj				=  ##%% ESCRIBIR EL CODIGO AQUI
+        aj				=  Wh @ Xm + bh*np.ones((Ni, 1))
         [y, dfh]		= activation(aj)
         # Calcular ahora el valor de salida utilizando lo precedente
-        ak				=  ##%% ESCRIBIR EL CODIGO AQUI
+        ak				=  Wo @ y + bo
         [zk, dfo]	= activation(ak);
         # Ya se puede calcular la salida y el error cometido
         # Evaluar ahora el delta_k a la salida: 
-        delta_k		=  ##%% ESCRIBIR EL CODIGO AQUI
+        delta_k		=  (tk - zk)*dfo
         #...y delta_j: 
-        delta_j		=  ## %% ESCRIBIR EL CODIGO AQUI
+        delta_j		= dfh * (Wo.T @ delta_k)
         #% Ahora se actualizan los pesos
         ## los pesos de la capa de salida
-        Wo				=  ##; %% ESCRIBIR CODIGO AQUI
+        Wo				=  Wo + eta * delta_k @ y.T
         ##% los pesos de la capa escondida
-        Wh				=  ##; %% ESCRIBIR CODIGO AQUI
+        Wh				=  Wh + eta * delta_j @ Xm.T
             
     #Calculate total error
     J[m]    = 0;
     for i in range(M): 
         Xm = X[:,i].reshape((Ni,1))
-		aj =  ## %% ESCRIBIR EL CODIGO AQUI
+        aj =  Wh @ Xm + bh*np.ones((Ni, 1))
         [y, dfh]		= activation(aj)
-		ak =  ## %% ESCRIBIR EL CODIGO AQUI
-        [zk, dfo]	= activation(ak);
-        J[m] = J[m] + (T[i] - zk)**2;
+        ak =  Wo @ y + bo
+        [zk, dfo]	= activation(ak)
+        J[m] = J[m] + (T[i] - zk)**2
     
-    J[m] = J[m]/M; 
+    J[m] = J[m]/M
     print('Iteracion %d: Error Total %f' % (m, J[m]))
-    m = m + 1;
+    m = m + 1
 
 # El error a la salida debe ser aproximadamente 0.00071965
-expErr = 0.00071965;
+expErr = 0.00071965
 assert(np.abs(J[epochs-1]-expErr) < 1e-6)#, 'Error de implementacion')
     
 # Resultado
