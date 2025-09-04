6a945284db6656563891aea8c618c8c4d8af89d2
SantiDrelewicz <santidrelewicz2000@gmail.com>
2025-09-04 02:15:43 -0300

    add clase 3

---
+++ Métodos Avanzados de Síntesis y Procesamiento de Imágenes/Clase 3 - Redes Convolucionales/Recursos Taller 3/Laboratorio 03/ejercicios/pooling.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def cnnPool(poolDim, convolvedFeatures):
+#%  cnnPool Pools los descriptores provenientes de la convolucion
+#%  La funcion usa el Pool PROMEDIO
+#% Parametros:
+#%  poolDim - dimension de la regiom de pool
+#%  convolvedFeatures - los descriptores a realizar el pool 
+#%                      convolvedFeatures(imageRow, imageCol, featureNum, imageNum)
+#%
+#% Devuelve:
+#%  pooledFeatures - matriz de los features agrupados
+#%                   pooledFeatures(poolRow, poolCol, featureNum, imageNum)
+#%     
+
+    numImages = convolvedFeatures.shape[3]
+    numFilters = convolvedFeatures.shape[2]
+    convolvedDim = convolvedFeatures.shape[0] # me sirve usar uno ya que las imagenes son cuadradas
+
+    px, py = int(convolvedDim / poolDim), int(convolvedDim / poolDim)
+    pooledFeatures = np.zeros((px, py, numFilters, numImages))
+
+#% Instrucciones:
+#%   Realizar el pool de los features en regiones de tamaño poolDim x poolDim,
+#%   para obtener la matriz pooledFeatures de 
+#%   (convolvedDim/poolDim) x (convolvedDim/poolDim) x numFeatures x numImages 
+
+#%   pooledFeatures(poolRow, poolCol, featureNum, imageNum) es el valor 
+#%   del descriptor featureNum de la imagen imageNum agrupada sobre la 
+#%   region (poolRow, poolCol). 
+#%   
+
+#%%% IMPLEMENTAR AQUI %%%
+
+    return pooledFeatures
+
